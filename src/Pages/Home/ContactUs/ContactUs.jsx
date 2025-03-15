61b142ed594ed37f5aac347904b8cecf2e5d0288
Tahsin974 <tahsinislam974@gmail.com>
2025-03-15 13:28:45 +0600

    home page done

--- src/Pages/Home/ContactUs/ContactUs.jsx
+++ src/Pages/Home/ContactUs/ContactUs.jsx
@@ -19,11 +19,19 @@ const ContactUs = () => {
   return (
     <div className="hero bg-[#000000] text-white  py-12 my-32">
       <div className="hero-content text-center ">
-        <div>
-          <div className="flex lg:text-7xl md:text-6xl sm:text-5xl text-2xl font-bold gap-4">
-            <h1>Let's</h1>{" "}
-            <span className="text-animation">{texts[index]}</span>
+        <div className=" lg:flex lg:gap-8 gap-5 items-center justify-items-center">
+          <div className="flex lg:text-7xl md:text-6xl sm:text-5xl text-2xl font-bold gap-4 ">
+            <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-600">
+              Let's
+            </h1>{" "}
+            <span className="text-animation bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-600">
+              {texts[index]}
+            </span>
           </div>
+          <div className=" border-b border-2 border-white mx-4 w-40"></div>
+          <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl btn-ghost  hover:bg-transparent hover:border-transparent hover:shadow-none text-fuchsia-50 font-bold text-6xl">
+            send message
+          </button>
         </div>
       </div>
     </div>
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { FaFingerprint } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router";
 
@@ -105,7 +106,34 @@ const Navbar = ({ children }) => {
       />
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
-        <div className="navbar bg-transparent absolute z-20 text-white my-8 animation">
+        <div className="navbar bg-transparent absolute z-20 text-white my-8 items-center justify-center ">
+          <div className="navbar-start">
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="my-drawer-3"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost"
+              >
+                <GiHamburgerMenu size={25} />
+              </label>
+            </div>
+            <Link className="btn btn-ghost hover:bg-transparent hover:border-transparent hover:shadow-none lg:text-3xl md:text-3xl text-xl">
+              Studio Vibe
+            </Link>
+          </div>
+          <div className="navbar-center animation hidden lg:flex">
+            <ul className="px-1 menu menu-horizontal text-xl font-bold space-x-3">
+              {navOptions}
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl bg-gradient-to-r from-[#3337E0]  to-[#BE224D] text-white rounded-3xl">
+              <FaFingerprint />
+              Client Login
+            </button>
+          </div>
+        </div>
+        {/* <div className="navbar bg-transparent absolute z-20 text-white my-8 items-center">
           <div className="flex-none lg:hidden">
             <label
               htmlFor="my-drawer-3"
@@ -115,12 +143,20 @@ const Navbar = ({ children }) => {
               <GiHamburgerMenu size={25} />
             </label>
           </div>
-          <div className="hidden mx-auto flex-none lg:block">
+          <div>
+            <h1 className="navbar-start text-2xl font-bold">Studio Vibe</h1>
+          </div>
+          <div className="navbar-center hidden  lg:flex animation">
             <ul className="menu menu-horizontal text-xl font-bold space-x-3">
               {navOptions}
             </ul>
           </div>
-        </div>
+          <div className="navbar-end">
+            <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+              Responsive
+            </button>
+          </div>
+        </div> */}
         {/* Page content here */}
         {children}
       </div>
